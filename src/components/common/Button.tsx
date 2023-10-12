11b1886c6b84d684c8d2d32aa1de51d00150604f
kimjh11130 <kimjh11130@naver.com>
2023-10-12 11:26:54 +0900

    fix: Button props 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -21,22 +21,22 @@ const Wrapper = styled.button<{ kind: string }>`
   border-radius: 20px;
   font-size: 28px;
   font-weight: 600;
-  color: ${({ color, theme }) =>
-    color === 'light' ? theme.colors.purple[800] : theme.colors.gray[100]};
-  background-color: ${({ color, theme }) =>
-    color == 'light' ? theme.colors.gray[100] : theme.colors.purple[700]};
+  color: ${({ kind, theme }) =>
+    kind === 'light' ? theme.colors.purple[800] : theme.colors.gray[100]};
+  background-color: ${({ kind, theme }) =>
+    kind == 'light' ? theme.colors.gray[100] : theme.colors.purple[700]};
   padding: 16px 54px;
   transition: all 0.1s linear;
   &:hover {
-    color: ${({ color, theme }) =>
-      color === 'light' ? theme.colors.purple[900] : theme.colors.gray[200]};
-    background-color: ${({ color, theme }) =>
-      color == 'light' ? theme.colors.gray[200] : theme.colors.purple[800]};
+    color: ${({ kind, theme }) =>
+      kind === 'light' ? theme.colors.purple[900] : theme.colors.gray[200]};
+    background-color: ${({ kind, theme }) =>
+      kind == 'light' ? theme.colors.gray[200] : theme.colors.purple[800]};
   }
   &:active {
-    color: ${({ color, theme }) =>
-      color === 'light' ? theme.colors.purple[700] : theme.colors.gray[50]};
-    background-color: ${({ color, theme }) =>
-      color == 'light' ? theme.colors.gray[50] : theme.colors.purple[600]};
+    color: ${({ kind, theme }) =>
+      kind === 'light' ? theme.colors.purple[700] : theme.colors.gray[50]};
+    background-color: ${({ kind, theme }) =>
+      kind == 'light' ? theme.colors.gray[50] : theme.colors.purple[600]};
   }
 `;
